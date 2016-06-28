56c9cb6594da77e6e41d71da581391652a051a16
D.A Jalloh <info@codewithjalloh.com>
2016-06-28 19:48:14 +0100

    outlet added

--- JSONParse/ViewController.swift
+++ JSONParse/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Outlet
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var myDescription: UITextView!
+    
+    var dataJSON = NSMutableData()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let strURL:NSString = "https://itunes.apple.com/gb/rss/topfreeapplications/limit=100/json"
+        
+        // url
+        let url = NSURL(string: strURL as String)
+        
+        // NSURLRequest
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
         
-        //https://itunes.apple.com/gb/rss/topfreeapplications/limit=100/json
+        //
     }
 
 
